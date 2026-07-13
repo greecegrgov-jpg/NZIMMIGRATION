08ca1074c6ede82865431ee84e678d9728952db5
greecegrgov-jpg <greece.gr.gov@gmail.com>
2026-07-13 18:31:52 +0500

    Update data.js

--- data.js
+++ data.js
@@ -2,18 +2,32 @@ const visaRecords = [
 
 {
     familyName: "Husnain Ali",
-
     nationality: "Pakistan",
-
     passportNumber: "SU1819363",
-
     dob: "1993-01-31",
-
     gender: "Male",
-
     visaStartDate: "2026-07-13",
-
     document: "documents/visa001748972.pdf"
+},
+
+{
+    familyName: "John Smith",
+    nationality: "United Kingdom",
+    passportNumber: "UK987654",
+    dob: "1988-10-15",
+    gender: "Male",
+    visaStartDate: "2026-08-01",
+    document: "documents/visa002.pdf"
+},
+
+{
+    familyName: "Sara Khan",
+    nationality: "Pakistan",
+    passportNumber: "PK445566",
+    dob: "1995-02-10",
+    gender: "Female",
+    visaStartDate: "2026-09-20",
+    document: "documents/visa003.pdf"
 }
 
 ];
