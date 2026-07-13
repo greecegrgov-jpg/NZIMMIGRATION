9ddc7158d12c99aaff10998d64cd577c7885465d
greecegrgov-jpg <greece.gr.gov@gmail.com>
2026-07-13 19:27:38 +0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,49 @@
+// ======================================
+// ADMIN LOGIN
+// ======================================
 
+document.addEventListener("DOMContentLoaded", function () {
+
+    const loginButton = document.getElementById("loginBtn");
+
+    loginButton.addEventListener("click", login);
+
+});
+
+
+// ======================================
+// LOGIN FUNCTION
+// ======================================
+
+function login() {
+
+    const username = document.getElementById("username").value.trim();
+
+    const password = document.getElementById("password").value.trim();
+
+    const message = document.getElementById("message");
+
+
+    if (username === "admin" && password === "123456") {
+
+        message.style.color = "green";
+
+        message.innerHTML = "Login successful...";
+
+        setTimeout(function () {
+
+            window.location.href = "dashboard.html";
+
+        }, 800);
+
+    }
+
+    else {
+
+        message.style.color = "red";
+
+        message.innerHTML = "Incorrect username or password.";
+
+    }
+
+}
